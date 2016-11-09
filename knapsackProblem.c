1bd1b850d4137401cade873d4a69d550e2eeff74
Mustafa Can <l1113077@std.yildiz.edu.tr>
2016-11-09 20:31:30 +0200

    code beautify

--- knapsackProblem.c
+++ knapsackProblem.c
@@ -1,42 +1,46 @@
-#include "stdlib.h"
 #include "stdio.h"
+#include "stdlib.h"
 
 int max(int a,int b);
 
-int knapSack(int capacity,int wt[],int val[],int n);
+int knapsack(int maxWeight,int weight[],int weightIndex,int val[],int valIndex);
 
 int main(int argc, char const *argv[])
 {
     int val[] = {60, 100, 120};
     int wt[] = {10, 20, 30};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("%d", knapSack(W, wt, val, n));
+    int  maxWeight = 50;
+    int valIndex=2;
+    int weightIndex=2;
+
+    printf("%d\n", knapsack(maxWeight,wt,weightIndex,val,valIndex)); 
 	return 0;
 }
 
 int max(int a,int b)
 {
-	return (a>b) ? a : b;
+	return (a>b) ? a : b ;
 }
 
-int knapSack(int capacity,int wt[],int val[],int n)
+int knapsack(int maxWeight,int weight[],int weightIndex,int val[],int valIndex)
 {
+	int dp[valIndex+1][maxWeight+1];
 	int i;
 	int j;
-	int K[n+1,capacity+1];
 
-	for (i = 0; i <=n; i++)
+	for (i = 0; i <= valIndex; i++)
 	{
-		for (j = 0; j <= capacity; j++)
+		for (j = 0; j <=maxWeight ; j++)
 		{
-			if(j==0 || i ==0)
-				K[i][j]=0;
-			else if(wt[i-1]<=j)
-				K[i][j]=max(val[i-1]+K[i-1][j-wt[i-1]],K[i-1][j]);
+			if (j==0)
+               dp[i][j] = 0;
+			else if(weight[i]<=maxWeight)
+				dp[i][j]=max(val[i]+dp[i-1][maxWeight-weight[i]],dp[i-1][j]);
 			else
-				K[i][j]=K[i-1][j];
-		}
+				dp[i][j]=dp[i-1][j];
+
+		}	
 	}
-	return K[n][capacity];
+
+	return dp[weightIndex][maxWeight];
 }
